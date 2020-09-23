b452a4a06f20d387be98c7c4127417f81421dc3a
Lupo Pasini Veronica <v.lupopasini@esconsulting.it>
2020-09-23 17:10:20 +0200

    hot reload

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
 <div>
 <h1>Hello world</h1>
+<p>New paragraph</p>
 </div>
 </template>
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 'use strict'
+const webpack = require('webpack')
 const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   mode: 'development',
+  
   entry: [
     './src/app.js'
   ],
+  devServer: {
+    hot: true,
+    watchOptions: {
+      poll: true
+    }
+  },
   module: {
     rules: [
       {
@@ -14,6 +23,12 @@ module.exports = {
     ]
   },
   plugins: [
-    new VueLoaderPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    })
   ]
 }
